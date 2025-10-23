2f320c0a9ddae181a80d66a50eb4f2281e1683d3
Luan Victor <lu.victor.2005@gmail.com>
2025-10-23 19:02:01 -0300

    Register Models in Admin Panel

--- portal_estudos/admin.py
+++ portal_estudos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Disciplina, Material
 
 # Register your models here.
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+    
+    search_fields = ('nome',)
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'disciplina', 'usuario', 'data_upload')
+    
+    list_filter = ('disciplina', 'usuario', 'data_upload')
+    
+    search_fields = ('titulo', 'descricao')
+    
+    autocomplete_fields = ['usuario', 'disciplina']
+    
